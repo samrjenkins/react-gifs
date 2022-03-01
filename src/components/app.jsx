1ba76161d54b0acf41023e392c102aeaebacef0b
Sam Jenkins <sam.jenko@hotmail.com>
2022-03-01 21:17:03 +0000

    initial attempt

---
+++ src/components/app.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import FormSearch from './form_search'
+import Gif from './gif'
+import GifList from './gif_list'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { selectedGifId: null, gifs: [] }
+  }
+
+  render = () => {
+    return (
+      <div>
+        <div className="left-scene">
+          <FormSearch changeHandler={this.handleFormSearchChange} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} gifClickHandler={this.handleGifClick} />
+        </div>
+      </div>
+    )
+  }
+
+  handleGifClick = (evt) => {
+    this.setState({ selectedGifId: evt.target.dataset.gifId})
+  }
+
+  handleFormSearchChange = (err, res) => {
+    this.setState({ gifs: res.data })
+  }
+}
+
+export default App
---
+++ src/components/gif.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Gif extends Component {
+  render = () => <img className="gif" src={this.src()} onClick={this.props.clickHandler} data-gif-id={this.props.id} />
+
+  src = () => `https://media.giphy.com/media/${this.props.id}/giphy.gif`
+}
+
+export default Gif
